d708b6387153b0ae3299d9af4131ca13ba2e7630
lesnah27 <lesnah27@yahoo.com>
2016-12-05 20:12:25 -0400

    Mi primera Version De Crystal Repor

---
+++ TareaIIIB/TareaIIIB/Reportes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaIIIB
+{
+    public partial class Reportes : Form
+    {
+        public Reportes()
+        {
+            InitializeComponent();
+        }
+
+        private void crv_Load(object sender, EventArgs e)
+        {
+            DsReporteEmpleadosTableAdapters.empleadosTableAdapter tap = new DsReporteEmpleadosTableAdapters.empleadosTableAdapter();
+            DsReporteEmpleados ds = new DsReporteEmpleados();
+            tap.Fill(ds.empleados);
+
+            rptEmpleados rpt = new rptEmpleados();
+            rpt.SetDataSource(ds);
+
+            crv.ReportSource = rpt;
+
+        }
+    }
+}
